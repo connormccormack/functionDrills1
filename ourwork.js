a1cd08d3dde04f8139a40fe0f6a75bfd50df7781
Connor McCormack <mccormack.connor.m@gmail.com>
2020-02-10 15:46:18 -0600

    thru #4

--- ourwork.js
+++ ourwork.js
@@ -1,6 +1,6 @@
 'use strict'
-function createGreeting() {
-  return 'My name is Connor and I am 26 years young';
+function createGreeting(name, age) {
+  return `My name is ${name} and I am ${age} years young`;
 };
-const greeting = createGreeting();
+const greeting = createGreeting('john', 12);
 console.log(greeting);

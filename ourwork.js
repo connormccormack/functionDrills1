c17f1a259f5c7957f4acfba3ab132df35840690b
Connor McCormack <mccormack.connor.m@gmail.com>
2020-02-10 16:31:00 -0600

    first 7 working

--- ourwork.js
+++ ourwork.js
@@ -1,6 +1,22 @@
 'use strict'
 function createGreeting(name, age) {
-  return `My name is ${name} and I am ${age} years young`;
+  const birthYear = yearOfBirth(age);
+  return `My name is ${name} and I am ${age} years young.  I was born in ${birthYear}`;
 };
-const greeting = createGreeting('john', 12);
+
+function yearOfBirth(age) {
+  
+  if (age < 0) {
+    throw new Error ("age cannot be negative");
+  } 
+  return 2020 - age;
+}
+
+try {
+  const greeting2 = createGreeting('john', -60);
+} catch(e) {
+  console.error(e.message);
+}
+
+const greeting = createGreeting('cain', 10);
 console.log(greeting);

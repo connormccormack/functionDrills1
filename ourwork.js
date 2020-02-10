4e66ff46a63a07a59785b39c5e0573a12bd88592
Brannen Petit <bpetit940@gmail.com>
2020-02-10 15:33:51 -0600

    second commit added function

--- ourwork.js
+++ ourwork.js
@@ -1,1 +1,6 @@
-console.log('My name is Connor and I am 26 years young');
+'use strict'
+function createGreeting() {
+  console.log('My name is Connor and I am 26 years young');
+};
+
+createGreeting();
